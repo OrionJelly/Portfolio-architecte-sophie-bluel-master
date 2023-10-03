a84441421aca0038a3648a2e8a240dc04e90a040
OrionJelly <91801372+OrionJelly@users.noreply.github.com>
2023-10-03 12:58:22 +0200

    feat: Ajout Verif Formulaire/Style/Commentaires

--- FrontEnd/script/login.js
+++ FrontEnd/script/login.js
@@ -1,37 +1,42 @@
+/*********************************************************************************
+**********************************************************************************
+***********************    PROCEDURE GLOBALE    **********************************
+==> Suppression comportement par defaut du bouton submit
+==> Vérification de la validité des données du formulaire 
+==> Envoi du formulaire de connection + Récupération de la réponse du serveur
+==> Sauvegarde de l'userId et du token d'authentification  dans le localstorage
+==> Redirection vers la page d'accueil
+********************************************************************************** 
+**********************************************************************************/
 const formEl = document.getElementById("formlogin");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 const error = document.getElementById("error");
+const submitButton = document.querySelector(".btn-connexion");
+submitButton.classList.add("btn-connexion__hidden");
 
-// const submitHandler = async (e) => {
-//   try {
-//     e.preventDefault();
-//     const formData = new FormData(formEl);
-//     const form = new URLSearchParams(formData);
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-//     const res = await fetch("http://localhost:5678/api/users/login", {
-//       method: "POST",
-//       headers: {
-//         accept: "application/json",
-//       },
-//       body: form,
-//     });
-//     const data = await res.json();
-//     if (res.ok) {
-//       localStorage.setItem("token", data.token);
-//       localStorage.setItem("userId", data.userId);
-//       location.href = "./index.html";
-//     } else {
-//       error.innerText = " Erreur dans l’identifiant ou le mot de passe";
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+/****Initialisation de la phase de connection ****/
 
-// formEl.addEventListener("submit", submitHandler);
+formEl.addEventListener("input", () => {
+  verifyEmail();
+  verifyPassword();
+  if (verifyEmail() === true && verifyPassword() === true) {
+    submitButton.classList.remove("btn-connexion__hidden");
+    formEl.addEventListener("submit", submitHandler);
+  } else {
+    error.innerText = "Veuillez renseigner les champs correctement";
+  }
+});
 
-formEl.addEventListener("submit", async function submitHandler(e) {
+/**** Fin de la phase de connection ****/
+
+/**** Fonction qui permet envoi du formulaire et récupération de la réponse serveur ****/
+
+async function submitHandler(e) {
   e.preventDefault();
   const formData = new FormData(formEl);
   const form = new URLSearchParams(formData);
@@ -52,4 +57,33 @@ formEl.addEventListener("submit", async function submitHandler(e) {
   } catch (error) {
     console.log(error);
   }
-});
+}
+
+/**** Fonctions qui permettent la vérification des données du formulaire
+ ****/
+
+function verifyEmail() {
+  emailInput.classList.remove("regex-valid");
+  if (
+    emailInput.value.match(
+      /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
+    )
+  ) {
+    emailInput.classList.add("regex-valid");
+    return true;
+  } else {
+    console.log("pas bon");
+    return false;
+  }
+}
+
+function verifyPassword() {
+  passwordInput.classList.remove("regex-valid");
+  if (passwordInput.value.match(/^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{5,}$/)) {
+    passwordInput.classList.add("regex-valid");
+    return true;
+  } else {
+    console.log("pas ok");
+    return false;
+  }
+}
